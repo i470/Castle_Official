ae51c6b858edff8b520eb35003eb25bc1cb32dd0
nashanas <shagolo@gmail.com>
2018-08-11 00:59:47 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -173,8 +173,8 @@ public:
         vSeeds.push_back(CDNSSeedData("209.250.254.252", "209.250.254.252"));   // Single node address
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 56);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 46);
-        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 212);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 50);
+        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 184);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x02)(0x2D)(0x25)(0x33).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x02)(0x21)(0x31)(0x2B).convert_to_container<std::vector<unsigned char> >();
         // 	BIP44 coin type is from https://github.com/satoshilabs/slips/blob/master/slip-0044.md
